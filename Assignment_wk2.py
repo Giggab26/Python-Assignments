487e64bfde33dd80710e0845745eec63857352bd
JUMA MAXIWELL <jumamaxwell185@gmail.com>
2025-08-14 16:59:33 +0300

    add Assignment_wk2.py

---
+++ Assignment_wk2.py
@@ -0,0 +1,20 @@
+# Empty list
+My_list = []
+
+# Append elements
+My_list.append(10)
+My_list.append(20)
+My_list.append(30)
+My_list.append(40)
+
+
+My_list.insert(1, 15)
+
+My_list.extend([50, 60, 70])
+
+My_list.pop()
+
+index_30 = My_list.index(30)
+print("Index of 30:", index_30)
+
+print("Final list:", My_list)
